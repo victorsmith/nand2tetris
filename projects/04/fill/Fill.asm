4aa7e403fb3e25e308731345ec06f37a7cf1df18
victorsmith <victor7smith@gmail.com>
2023-05-08 20:02:07 -0600

    minor change

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,66 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// Put your code here
+  @8192
+  D=A
+  @i
+  M=D
+(LOOP)
+  @KBD
+  D=M
+  // If KBD !=  0; goto BLACK
+  @BLACK
+  D;JNE
+  @WHITE
+  0;JMP
+
+(BLACK)
+  // If i == 0 => goto LOOP
+  @i
+  D=M
+  @RESET
+  D;JEQ
+  // Else => set *(SCREEN + i) = -1 && decrement n by 1
+  @SCREEN
+  D=A
+  @i
+  A=M+D
+  M=-1
+  @i
+  M=M-1
+  // Restart the loop
+  @BLACK
+  0;JMP
+
+(WHITE)
+   // If i == 0 => goto LOOP
+  @i
+  D=M
+  @RESET
+  D;JEQ
+  // Else => set *(SCREEN + i) = -1 && decrement n by 1
+  @SCREEN
+  D=A
+  @i
+  A=M+D
+  M=0
+  @i
+  M=M-1
+  // Restart the loop
+  @WHITE
+  0;JMP
+
+
+(RESET)
+  @8192
+  D=A
+  @i
+  M=D
+
+  @LOOP
+  0;JMP
+
+
+
+
